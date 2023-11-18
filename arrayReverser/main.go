d4ca6356d64d961e89f4d5db63aa31a2ebfbae0e
Stanislav <abettor23@gmail.com>
2023-11-18 14:02:29 +0300

    first version of the program

---
+++ arrayReverser/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func reverser(arr [10]int) [10]int {
+	var reversedArr [10]int
+	for i, j := len(arr)-1, 0; i >= 0 && j < len(reversedArr); i, j = i-1, j+1 {
+		reversedArr[j] = arr[i]
+	}
+	return reversedArr
+}
+
+func main() {
+	fmt.Println("Введите 10 целых чисел, а я их переверну!")
+
+	var numbers [10]int
+
+	for i := 0; i < len(numbers); i++ {
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		n, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Ошибка: введите корректное целое число.")
+			continue
+		}
+		numbers[i] = n
+	}
+
+	result := reverser(numbers)
+
+	fmt.Println("Ваши десять чисел:", numbers)
+	fmt.Println("Ваши числа, но перевёрнутые:", result)
+}
---
+++ evenOddCalc/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Введите 10 целых чисел, а я из них выведу количество чётных и нечётных:")
+
+	var numbers [10]int
+	var evens int
+	var odds int
+
+	for i := 0; i < len(numbers); i++ {
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		n, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Ошибка: введите корректное целое число.")
+			continue
+		}
+		numbers[i] = n
+	}
+
+	for i, _ := range numbers {
+		if numbers[i]%2 == 0 {
+			evens++
+		} else {
+			odds++
+		}
+	}
+	fmt.Println("Все ваши числа:", numbers)
+	fmt.Println("Количество чётных чисел:", evens)
+	fmt.Println("Количество нечётных чисел:", odds)
+}
